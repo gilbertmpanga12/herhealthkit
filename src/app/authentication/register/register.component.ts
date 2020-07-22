83dc895a6a5aac720a73bb7257bf5c7af68503f8
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-22 22:00:54 +0300

    Update; user account view setup

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  accountSelected: string = null;
   accountTypes: string[] = ['User account', 'Registration Center Account', 'Physician Account'];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectAccountType(selectedType: string): void{
+    this.accountSelected = selectedType;
+  }
+  
+  unselectAccountType(): void{
+    this.accountSelected = null;
   }
 
 }
