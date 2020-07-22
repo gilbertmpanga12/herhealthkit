8259714f55c864f94299f13b53b98fe9d10f8b08
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-22 22:40:40 +0300

    Update;- ui completed for authentication

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthenticationComponent } from './authentication.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 
 const routes: Routes = [{ path: '', component: AuthenticationComponent },
-{path: 'register', component: RegisterComponent}
+{path: 'register', component: RegisterComponent},
+{path: 'forgot-password', component: ForgotpasswordComponent}
 ];
 
 @NgModule({
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -6,10 +6,11 @@ import { AuthenticationComponent } from './authentication.component';
 import { RegistrationcenterComponent } from './registrationcenter/registrationcenter.component';
 import { PhysicianaccountComponent } from './physicianaccount/physicianaccount.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 
 
 @NgModule({
-  declarations: [AuthenticationComponent, RegistrationcenterComponent, PhysicianaccountComponent, RegisterComponent],
+  declarations: [AuthenticationComponent, RegistrationcenterComponent, PhysicianaccountComponent, RegisterComponent, ForgotpasswordComponent],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
