d10aa50cc4d03cecca58b8948be66cc93492b675
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-22 21:13:22 +0300

    Update;- ui for account type added

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  accountTypes: string[] = ['User account', 'Registration Center Account', 'Physician Account'];
   constructor() { }
 
   ngOnInit(): void {
