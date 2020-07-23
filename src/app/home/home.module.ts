53cbd7963db609268d4e4bf3c5ddc83d49f5081a
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-23 21:27:19 +0300

    Update;- deleted useless component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,13 +5,12 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ResultsComponent } from './results/results.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
-import { CheckEmailConfirmationComponent } from './check-email-confirmation/check-email-confirmation.component';
 import { EmailnotverifiedComponent } from './emailnotverified/emailnotverified.component';
 import { TaketestComponent } from './taketest/taketest.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, ResultsComponent, ColorPickerComponent, CheckEmailConfirmationComponent, EmailnotverifiedComponent, TaketestComponent],
+  declarations: [HomeComponent, ResultsComponent, ColorPickerComponent, EmailnotverifiedComponent, TaketestComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
