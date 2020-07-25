36b9d531cf613cbcaa9466543007ece4e13a7bcc
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-25 15:54:55 +0300

    Update;- added environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAweFXOlzs0SIxJ38LoaJggKlFLq6PmWSg",
+    authDomain: "momets-4e5e3.firebaseapp.com",
+    databaseURL: "https://momets-4e5e3.firebaseio.com",
+    projectId: "momets-4e5e3",
+    storageBucket: "momets-4e5e3.appspot.com",
+    messagingSenderId: "968650247929",
+    appId: "1:968650247929:web:96c86bb9ff9d2fd0989c28"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAweFXOlzs0SIxJ38LoaJggKlFLq6PmWSg",
+    authDomain: "momets-4e5e3.firebaseapp.com",
+    databaseURL: "https://momets-4e5e3.firebaseio.com",
+    projectId: "momets-4e5e3",
+    storageBucket: "momets-4e5e3.appspot.com",
+    messagingSenderId: "968650247929",
+    appId: "1:968650247929:web:96c86bb9ff9d2fd0989c28"
+  }
 };
 
 /*
