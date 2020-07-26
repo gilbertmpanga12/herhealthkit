fcf766b4ee61b9bc9ce5a9d2314d9b0731aed0f7
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-26 14:20:44 +0300

    Update;- completed registration center account styles & form creation

--- src/app/authentication/registrationcenter/registrationcenter.component.ts
+++ src/app/authentication/registrationcenter/registrationcenter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from 'src/app/main.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrationcenter',
@@ -7,10 +8,23 @@ import { MainService } from 'src/app/main.service';
   styleUrls: ['./registrationcenter.component.scss']
 })
 export class RegistrationcenterComponent implements OnInit {
-
-  constructor(public service: MainService) { }
+  registrationCenterGroup: FormGroup;
+  constructor(public service: MainService, private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registrationCenterGroup = this._fb.group({
+      country: ['Uganda', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      category: ['Hospital', Validators.required],
+      district: ['', Validators.required],
+      city: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  createRegistrationCenterAccount(): void{
+    this.service.isLoading = true;
   }
 
 }
