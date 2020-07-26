52562145930df131c8b72802ecc00e31b9b8bea8
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-26 15:19:17 +0300

    Update;- completed forms styling

--- src/app/authentication/physicianaccount/physicianaccount.component.ts
+++ src/app/authentication/physicianaccount/physicianaccount.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from 'src/app/main.service';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-physicianaccount',
@@ -13,7 +13,15 @@ export class PhysicianaccountComponent implements OnInit {
 
   ngOnInit(): void {
     this.physicialFormGroup = this._fb.group({
-
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      city: ['', Validators.required],
+      district: ['', Validators.required],
+      telephone: ['', Validators.required],
+      password: ['',[Validators.required, Validators.minLength(6)]],
+      destination: ['', Validators.required],
+      gender: ['Female', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
     });
   }
 
