64f54f0a21bca2adf42139a68d0233aeace4bd40
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-26 15:30:33 +0300

    Update;- email address reset forms configured

--- src/app/authentication/forgotpassword/forgotpassword.component.ts
+++ src/app/authentication/forgotpassword/forgotpassword.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MainService } from 'src/app/main.service';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgotpassword.component.scss']
 })
 export class ForgotpasswordComponent implements OnInit {
-
-  constructor() { }
+  forgotPasswordGroup: FormGroup;
+  constructor(public service: MainService, private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.forgotPasswordGroup = this._fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  requestNewPassword(): void{
+      this.service.isLoading = true;
+      const email = this.forgotPasswordGroup.get('email').value;
+      this.service.forgotPassword(email).then(res => {
+        this.service.isLoading = false;
+      }).catch(error => {
+        this.service.isLoading = false;
+        this.service.snackbar(error);
+      })
   }
 
 }
