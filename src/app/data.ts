dbf9b702bdb28089d54ea4a3b98b3b9db70ab29a
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-29 16:33:32 +0300

    Update; data payload set up for visual tests

---
+++ src/app/data.ts
@@ -0,0 +1,178 @@
+export const metrics = [
+  {
+      title: "LEU 120S",
+      parameters: [{name: "-", color:""},
+      {name: "15+", color:""},{name: "70+", color:""},
+      {name: "125++", color:""}, {name: "500++", color:""}]
+  },
+  {
+      title: "NIT 60S",
+      parameters: [
+          {name: "-", color: ""},
+          {name: "-", color: ""},
+          {name: "PINK ROSE", color: ""},
+          {name: "ROSAD", color: ""},
+          {name: "ROSAD ROSA", color: ""},
+          {name: "LYRESE ROD", color: ""},
+          {name: "", color: ""}
+      ]
+  }, 
+  {
+      title: "URO 60S",
+      parameters: [
+          {
+              name: "0.2(3.5)",
+              color: ""
+          },
+          {
+            name: "1(17)",
+            color: ""
+          },
+          {
+            name: "2(35)",
+            color: ""
+          },
+          {
+            name: "4(70)",
+            color: ""
+        },
+        {
+            name: "8(140)",
+            color: ""
+        },
+        {
+            name: "12(200)",
+            color: ""
+        },
+        {
+            name: "mg/dl(umo/L)",
+            color: ""
+        }
+      ]
+  }, 
+  {
+      title: "PRO 60S",
+      parameters: [
+          {
+              name: "-",
+              color: ""
+          },
+          {
+            name: "15(.15)+-",
+            color: ""
+        },
+        {
+            name: "30(0.3)+",
+            color: ""
+        },
+        {
+            name: "100(1.0)++",
+            color: ""
+        },
+        {
+            name: "300(3.0)+++",
+            color: ""
+        },
+        {
+            name: "2000(20)+++++",
+            color: ""
+        },
+        {
+            name: "mg/dL(g/L)",
+            color: ""
+        }
+      ]
+  },
+  {
+      title: "pH 60S",
+      parameters: [
+          {
+              name: "5.0",
+              color: ""
+          },
+          {
+              name: "6.0",
+              color: ""
+          },
+          {
+            name: "6.5",
+            color: ""
+        },
+        {
+            name: "",
+            color: ""
+        },
+        {
+            name: "7.0",
+            color: ""
+        },
+        {
+            name: "7.5",
+            color: ""
+        },
+        {
+            name: "8.0",
+            color: ""
+        },
+        {
+            name: "9.0",
+            color: ""
+        }
+      ]
+  },
+  {
+      title: "BLO 60S",
+      parameters: [
+          {name: "-", color: ""},
+          {name: "+-", color: ""},
+          {name: "+", color: ""},
+          {name: "++", color: ""},
+          {name: "+++", color: ""},
+          {name: "5-10", color: ""},
+          {name: "50-Ey/L", color: ""}
+      ]
+  },
+  {
+      title: "SG 45S",
+      parameters: [
+          {name: "1.000", color: ""},
+          {name: "1.000", color: ""},
+          {name: "1.005", color: ""},
+          {name: "1.010", color: ""},
+          {name: "1.015", color: ""},
+          {name: "1.020", color: ""},
+          {name: "1.025", color: ""}
+      ]
+  },
+  {
+      title: "KET 40S",
+      parameters: [
+          {name: "-", color: ""},
+          {name: "5(0,5)+-", color: ""},
+          {name: "15(1.5)+-", color: ""},
+          {name: "40(4.0)+-", color: ""},
+          {name: "80(8.0)+++", color: ""},
+          {name: "160(16)+++", color: ""}
+      ]
+  },
+  {
+      title: "BIL 30S",
+      parameters: [
+          {name: "-", color: ""},
+          {name: "1(17)+-", color: ""},
+          {name: "2(35)++", color: ""},
+          {name: "4(70)+++", color: ""}
+      ]
+  },
+  {
+      title: "GLU 30S",
+      parameters: [
+          {name: "-", color: ""},
+          {name: "100(5)+-", color: ""},
+          {name: "250(15)+", color: ""},
+          {name: "250(15)+", color: ""},
+          {name: "500(30)+_", color: ""},
+          {name: "1000(60)+++", color: ""}
+      ]
+  }
+];
