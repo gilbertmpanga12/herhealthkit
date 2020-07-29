8cd1305cb5e7ecf3f903e802b617efd08a7ac04d
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-29 19:56:13 +0300

    Update; colors set are dynamically set

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,20 +1,20 @@
-export const metrics = [
+export const Metrics = [
   {
       title: "LEU 120S",
-      parameters: [{name: "-", color:""},
-      {name: "15+", color:""},{name: "70+", color:""},
-      {name: "125++", color:""}, {name: "500++", color:""}]
+      parameters: [{name: "-", color:"#efebc6"},
+      {name: "15+", color:"#e8ddc9"},{name: "70+", color:"#dad4c6"},
+      {name: "125++", color:"#e7c6d9"}, {name: "500++", color:"#ad9fb9"}]
   },
   {
       title: "NIT 60S",
       parameters: [
-          {name: "-", color: ""},
-          {name: "-", color: ""},
-          {name: "PINK ROSE", color: ""},
-          {name: "ROSAD", color: ""},
-          {name: "ROSAD ROSA", color: ""},
-          {name: "LYRESE ROD", color: ""},
-          {name: "", color: ""}
+          {name: "-", color: "#fbf6e0"},
+          {name: "-", color: "#fee0e5"},
+          {name: "PINK ROSE", color: "#fafafa"},
+          {name: "ROSAD", color: "#fafafa"},
+          {name: "ROSAD ROSA", color: "#fafafa"},
+          {name: "LYRESE ROD", color: "#fafafa"},
+          {name: "", color: "#d86ca0"}
       ]
   }, 
   {
@@ -22,31 +22,31 @@ export const metrics = [
       parameters: [
           {
               name: "0.2(3.5)",
-              color: ""
+              color: "#fbd9be"
           },
           {
             name: "1(17)",
-            color: ""
+            color: "#f8cab7"
           },
           {
             name: "2(35)",
-            color: ""
+            color: "#f1a58e"
           },
           {
             name: "4(70)",
-            color: ""
+            color: "#d8868a"
         },
         {
             name: "8(140)",
-            color: ""
+            color: "#e17d95"
         },
         {
             name: "12(200)",
-            color: ""
+            color: "#e88395"
         },
         {
             name: "mg/dl(umo/L)",
-            color: ""
+            color: "#fafafa"
         }
       ]
   }, 
@@ -55,11 +55,11 @@ export const metrics = [
       parameters: [
           {
               name: "-",
-              color: ""
+              color: "#dce977"
           },
           {
             name: "15(.15)+-",
-            color: ""
+            color: "#bad36d"
         },
         {
             name: "30(0.3)+",
@@ -67,19 +67,20 @@ export const metrics = [
         },
         {
             name: "100(1.0)++",
-            color: ""
+            color: "#a5c177"
         },
         {
             name: "300(3.0)+++",
-            color: ""
+            color: "#90b991"
         },
         {
             name: "2000(20)+++++",
-            color: ""
+            color: "#70af9a"
+
         },
         {
             name: "mg/dL(g/L)",
-            color: ""
+            color: "#599c8a"
         }
       ]
   },
@@ -88,91 +89,88 @@ export const metrics = [
       parameters: [
           {
               name: "5.0",
-              color: ""
+              color: "#f1814a"
           },
           {
               name: "6.0",
-              color: ""
+              color: "#eca452"
           },
           {
             name: "6.5",
-            color: ""
+            color: "#dcc053"
         },
-        {
-            name: "",
-            color: ""
-        },
+        
         {
             name: "7.0",
-            color: ""
+            color: "#b5c15b"
         },
         {
             name: "7.5",
-            color: ""
+            color: "#81a54d"
         },
         {
             name: "8.0",
-            color: ""
+            color: "#4c9b6b"
         },
         {
             name: "9.0",
-            color: ""
+            color: "#1c7a7c"
         }
       ]
   },
   {
       title: "BLO 60S",
       parameters: [
-          {name: "-", color: ""},
-          {name: "+-", color: ""},
-          {name: "+", color: ""},
-          {name: "++", color: ""},
-          {name: "+++", color: ""},
-          {name: "5-10", color: ""},
-          {name: "50-Ey/L", color: ""}
+          {name: "-", color: "#e5be3f"},
+          {name: "+-", color: "#e6bd38"},
+          {name: "+", color: "#dbc946"},
+          {name: "++", color: "#c1bf44"},
+          {name: "+++", color: "#92ab45"},
+          {name: "5-10", color: "#578846"},
+          {name: "50-Ey/L", color: "#39533a"}
       ]
   },
   {
       title: "SG 45S",
       parameters: [
-          {name: "1.000", color: ""},
-          {name: "1.000", color: ""},
-          {name: "1.005", color: ""},
-          {name: "1.010", color: ""},
-          {name: "1.015", color: ""},
-          {name: "1.020", color: ""},
-          {name: "1.025", color: ""}
+          {name: "1.000", color: "#11422f"},
+          {name: "1.000", color: "#4d5926"},
+          {name: "1.005", color: "#737744"},
+          {name: "1.010", color: "#85894b"},
+          {name: "1.015", color: "#938006"},
+          {name: "1.020", color: "#a29b25"},
+          {name: "1.025", color: "#d8b50f"}
       ]
   },
   {
       title: "KET 40S",
       parameters: [
-          {name: "-", color: ""},
-          {name: "5(0,5)+-", color: ""},
-          {name: "15(1.5)+-", color: ""},
-          {name: "40(4.0)+-", color: ""},
-          {name: "80(8.0)+++", color: ""},
-          {name: "160(16)+++", color: ""}
+          {name: "-", color: "#f1ae93"},
+          {name: "5(0,5)+-", color: "#ec9995"},
+          {name: "15(1.5)+-", color: "#da877f"},
+          {name: "40(4.0)+-", color: "#cb726a"},
+          {name: "80(8.0)+++", color: "#945069"},
+          {name: "160(16)+++", color: "#6d3454"}
       ]
   },
   {
       title: "BIL 30S",
       parameters: [
-          {name: "-", color: ""},
-          {name: "1(17)+-", color: ""},
-          {name: "2(35)++", color: ""},
-          {name: "4(70)+++", color: ""}
+          {name: "-", color: "#fde7ae"},
+          {name: "1(17)+-", color: "#f8c89c"},
+          {name: "2(35)++", color: "#c3a485"},
+          {name: "4(70)+++", color: "#ac8e84"}
       ]
   },
   {
       title: "GLU 30S",
       parameters: [
-          {name: "-", color: ""},
-          {name: "100(5)+-", color: ""},
-          {name: "250(15)+", color: ""},
-          {name: "250(15)+", color: ""},
-          {name: "500(30)+_", color: ""},
-          {name: "1000(60)+++", color: ""}
+          {name: "-", color: "#97d0c7"},
+          {name: "100(5)+-", color: "#82c597"},
+          {name: "250(15)+", color: "#6aad4e"},
+          {name: "250(15)+", color: "#938006"},
+          {name: "500(30)+_", color: "#976831"},
+          {name: "1000(60)+++", color: "#7b502c"}
       ]
   }
 ];
--- src/app/home/color-picker/color-picker.component.ts
+++ src/app/home/color-picker/color-picker.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Metrics} from '../../data';
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
   styleUrls: ['./color-picker.component.scss']
 })
 export class ColorPickerComponent implements OnInit {
-
+  metrics: any = Metrics;
   constructor() { }
 
   ngOnInit(): void {
